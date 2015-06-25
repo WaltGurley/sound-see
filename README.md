a779cc75aa9162ce19f3b8ed555fb03f6afa2240
WaltGurley <walt.gurley@gmail.com>
2015-06-25 09:10:46 -0400

    added mic input

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 # SoundSee
-Web application for recording, playing, and visualizing sound.
-=======
-# SoundSee
-Web application for recording, playing, and visualizing sound. Built with AudioNode web API and D3.js 
->>>>>>> 74f654f3f763897dd25bbeac3af8b6528e7a41b9
+Web application for recording, playing, and visualizing sound. Built with AudioNode web API and D3.js
--- js/main.js
+++ js/main.js
@@ -1,22 +1,56 @@
 //AUDIO SETUP
+
+//setup and store audio context
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-
 var oscillator = audioCtx.createOscillator();
-var gainNode = audioCtx.createGain();
-var audioData = audioCtx.createAnalyser();
+var gainNodeOsc = audioCtx.createGain();
+var audioDataOsc = audioCtx.createAnalyser();
 
-oscillator.connect(gainNode);
-gainNode.connect(audioCtx.destination);
-gainNode.gain.value = 0.1
-oscillator.type = "sine";
-oscillator.start();
+var gainNodeMic = audioCtx.createGain();
+var audioDataMic = audioCtx.createAnalyser();
 
-setupControls();
+oscillator.connect(gainNodeOsc);
+gainNodeOsc.connect(audioCtx.destination);
+gainNodeOsc.gain.value = 0;
+gainNodeOsc.connect(audioDataOsc);
+oscillator.start(0);
+
+//get user media (i.e., microphone)
+navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
+   getUserMedia: function(c) {
+     return new Promise(function(y, n) {
+       (navigator.mozGetUserMedia ||
+        navigator.webkitGetUserMedia).call(navigator, c, y, n);
+     });
+   }
+} : null);
+
+//check if user media is available and start stream if so
+if (!navigator.mediaDevices) {
+  alert("getUserMedia() not supported.");
+} else {
+  var mic = navigator.mediaDevices.getUserMedia({ audio: true });
+
+  mic.then(function(stream) {
+    source = audioCtx.createMediaStreamSource(stream);
+    source.connect(audioDataMic);
+    audioDataMic.connect(gainNodeMic);
+    gainNodeMic.connect(audioCtx.destination);
+
+    gainNodeMic.gain.value = 0.2;
+  },
+
+  function(err) {
+     alert('The following error occured: ' + err);
+  });
+}
+
+setupOscillatorControls();
 setupVis();
 
-//CONTROLS SETUP
-function setupControls() {
+//OSCILLATOR CONTROL SETUP
+function setupOscillatorControls() {
   //list of oscillator types
   var oscTypes = ["sine", "sawtooth", "square", "triangle"];
 
@@ -31,18 +65,17 @@ function setupControls() {
     })
     .text(function(d) { return d; });
 
-  //make sine oscillator button active
-  d3.select(oscChoose[0][0]).classed("active", true);
-
   //change oscillator when oscillator button is clicked
   oscChoose.on("click", function(d) {
     var pressedBtn = d3.select(this);
 
     if (pressedBtn.classed("active")) {
       oscChoose.classed("active", false);
+      gainNodeOsc.gain.value = 0;
     } else {
       oscChoose.classed("active", false);
       pressedBtn.classed("active", true);
+      gainNodeOsc.gain.value = 0.05;
     }
 
     oscillator.type = d;
@@ -51,37 +84,87 @@ function setupControls() {
 
 //VISUALIZATIONS
 function setupVis() {
-  var visContainer = d3.select(".vis-container").append("svg");
 
-  oscillator.connect(audioData);
-  var bufferLength = audioData.frequencyBinCount;
-  var dataArray = new Uint8Array(bufferLength);
+  //read data from audio (bufferLength default 1024, UInt8Array returns 0-128)
+  var bufferLength = audioDataOsc.frequencyBinCount;
 
-  //control frequency and gain (i.e., volume) when mouse is moved in vis-container
+  //setup data for sources
+  var sourceData = {
+    "osc": [new Uint8Array(bufferLength), new Uint8Array(bufferLength)],
+    "mic": [new Uint8Array(bufferLength), new Uint8Array(bufferLength)]
+  };
+
+  //get time and frequency data from each sourceData
+  audioDataOsc.getByteTimeDomainData(sourceData.osc[0]);
+  audioDataMic.getByteTimeDomainData(sourceData.mic[0]);
+  audioDataOsc.getByteFrequencyData(sourceData.osc[1]);
+  audioDataMic.getByteFrequencyData(sourceData.mic[1]);
+
+  //create SVG
+  var visContainer = d3.select(".vis-container").append("svg"),
+    visWidth = parseInt(visContainer.style("width")),
+    visHeight = parseInt(visContainer.style("height"));
+
+  //control frequency when mouse is moved in vis-container
   visContainer.on("mousemove", function () {
     oscillator.frequency.value = d3.mouse(visContainer.node())[0] + 200;
-    gainNode.gain.value = 0.1 - (0.1 * d3.mouse(visContainer.node())[1] / window.innerHeight);
-    //console.log(oscillator.frequency.value, gainNode.gain.value);
-
-    //
   });
 
-  //d3 function for parsing line data
-  var makeLine = d3.svg.line()
-    .x(function(d,i) { return i; })
-    .y(function(d) { return d; });
+  //d3 function for parsing line data for waveform
+  var drawWaveform = d3.svg.line()
+    .x(function(d,i) { return i * visWidth / bufferLength; })
+    .y(function(d) { return (d - 128) + visHeight / 2; })
+    .interpolate("linear");
 
-  audioData.getByteTimeDomainData(dataArray);
-
-  var waveForm = visContainer.append("path")
-    .data(dataArray)
+  //create waveform paths for audio sources
+  var waveform = visContainer.selectAll("waveform")
+    .data(d3.entries(sourceData))
+    .enter()
+    .append("path")
     .attr({
-      "class": "waveform",
-      "d": makeLine(dataArray)
+      "class": function(d) { return "waveform " + d.key; },
+      "d": function(d) { return drawWaveform(d.value[0]); }
     });
 
-  setInterval(function() {
-    audioData.getByteTimeDomainData(dataArray);
-    waveForm.attr("d", makeLine(dataArray));
-  }, 250);
+  //d3 function for parsing line data for frequenct spectrum
+  var drawFreqSpectrum = d3.svg.line()
+    .x(function(d,i) { return i * visWidth / bufferLength; })
+    .y(function(d) { return visHeight - d; })
+    .interpolate("step");
+
+  //create frequency paths for audio sources
+  var freqSpectrum = visContainer.selectAll("freq-spec")
+    .data(d3.entries(sourceData))
+    .enter()
+    .append("path")
+    .attr({
+      "class": function(d) { return "freq-spec " + d.key; },
+      "d": function(d) { return drawFreqSpectrum(d.value[1]); }
+    });
+
+  //update visualization (get source audio data and redraw paths)
+  function updateVisualization() {
+    audioDataOsc.getByteTimeDomainData(sourceData.osc[0]);
+    audioDataMic.getByteTimeDomainData(sourceData.mic[0]);
+    audioDataOsc.getByteFrequencyData(sourceData.osc[1]);
+    audioDataMic.getByteFrequencyData(sourceData.mic[1]);
+
+    waveform
+      .data(d3.values(sourceData))
+      .attr("d", function(d) { return drawWaveform(d[0]); });
+
+    freqSpectrum
+      .data(d3.values(sourceData))
+      .attr("d", function(d) { return drawFreqSpectrum(d[1]); });
+  }
+
+  //run update on an interval
+  setInterval(updateVisualization, 50);
+
+  //Resize SVG elements on window resize
+  window.onresize = resized;
+  function resized() {
+    visWidth = parseInt(visContainer.style("width"));
+    visHeight = parseInt(visContainer.style("height"));
+  }
 }
