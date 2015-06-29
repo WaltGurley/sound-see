9a86370378c66ccfe47c63f055dc0ecc7abca9ef
Walt <walt.gurley@gmail.com>
2015-06-29 09:32:15 -0400

    add mic controls

--- js/main.js
+++ js/main.js
@@ -14,16 +14,12 @@ var audioDataOsc = audioCtx.createAnalyser();
 
 oscillator.connect(gainNodeOsc);
 gainNodeOsc.connect(audioCtx.destination);
-gainNodeOsc.gain.value = 0;
 gainNodeOsc.connect(audioDataOsc);
 var firstInteraction = true;
 
-//setup microphone
-var gainNodeMic = audioCtx.createGain();
-var audioDataMic = audioCtx.createAnalyser();
-
 //get user media (i.e., microphone)
-navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
+navigator.mediaDevices = navigator.mediaDevices ||
+  ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
    getUserMedia: function(c) {
      return new Promise(function(y, n) {
        (navigator.mozGetUserMedia ||
@@ -36,15 +32,22 @@ navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia |
 if (!navigator.mediaDevices) {
   alert("getUserMedia() not supported.\nCannot use microphone");
 } else {
+
+  //setup microphone
   var mic = navigator.mediaDevices.getUserMedia({ audio: true });
+  var gainNodeMic = audioCtx.createGain();
+  var audioDataMic = audioCtx.createAnalyser();
 
   mic.then(function(stream) {
     source = audioCtx.createMediaStreamSource(stream);
-    source.connect(audioDataMic);
-    audioDataMic.connect(gainNodeMic);
-    gainNodeMic.connect(audioCtx.destination);
+    source.connect(gainNodeMic);
+    gainNodeMic.connect(audioDataMic);
+    audioDataMic.connect(audioCtx.destination);
 
-    gainNodeMic.gain.value = 0.2;
+    gainNodeMic.gain.value = 0;
+
+    //setup microphone controls if mic available
+    setupMicControls();
   },
 
   function(err) {
@@ -52,63 +55,179 @@ if (!navigator.mediaDevices) {
   });
 }
 
+//always setup oscillator and visualization
 setupOscillatorControls();
 setupVis();
 
 //OSCILLATOR CONTROL SETUP
 function setupOscillatorControls() {
 
-  //list of oscillator types
-  var oscTypes = ["sine", "sawtooth", "square", "triangle"];
+  //initial oscillator output parameters
+  var volumeOsc = 0.1,
+    mutedOsc = false,
+    freqOsc = 440;
 
-  //create oscillator type buttons
-  var oscChoose = d3.select(".controls").selectAll("button")
-    .data(oscTypes)
-    .enter()
-    .append("button")
-    .attr({
-      "class": "col-xs-3 btn btn-default",
-      "type": "button"
-    })
-    .text(function(d) { return d; });
+  setupOscillatorButtons();
+  setupOscillatorSliders();
 
-  //change oscillator when oscillator button is clicked
-  oscChoose.on("click", function(d) {
-    var pressedBtn = d3.select(this);
+  //setup buttons to control waveform type (sine, sawtooth, triangle, square)
+  function setupOscillatorButtons() {
+    //list of oscillator types
+    var oscTypes = ["sine", "sawtooth", "square", "triangle"];
 
-    //start oscillator on first click of button (needed for iOS)
-    if (firstInteraction) {
-      oscillator.start(0);
-      firstInteraction = false;
-    }
+    //create oscillator type buttons
+    var oscChoose = d3.select(".osc-btns").selectAll("button")
+      .data(oscTypes)
+      .enter()
+      .append("button")
+      .attr({
+        "class": "col-xs-3 col-md-6 btn btn-default",
+        "type": "button"
+      })
+      .text(function(d) { return d; });
 
-    if (pressedBtn.classed("active")) {
-      oscChoose.classed("active", false);
-      gainNodeOsc.gain.value = 0;
-    } else {
-      oscChoose.classed("active", false);
-      pressedBtn.classed("active", true);
+    //change oscillator when oscillator button is clicked
+    oscChoose.on("click", function(d) {
+      var pressedBtn = d3.select(this);
 
-      gainNodeOsc.gain.value = 0.05;
-    }
+      //start oscillator on first click of button (needed for iOS)
+      if (firstInteraction) {
+        oscillator.start(0);
+        firstInteraction = false;
+      }
 
-    oscillator.type = d;
-  });
+      if (pressedBtn.classed("active")) {
+        oscChoose.classed("active", false);
 
-  var volumeSliderOsc = d3.select(".controls").append("input")
-    .attr({
-      "class": "osc-volume-slider",
-      "type": "range",
-      "min": 0,
-      "max": 1,
-      "step": 0.05,
-      "value": gainNodeOsc.gain.value
+        mutedOsc = true;
+        gainNodeOsc.gain.value = 0;
+      } else {
+        oscChoose.classed("active", false);
+        pressedBtn.classed("active", true);
+
+        mutedOsc = false;
+        gainNodeOsc.gain.value = volumeOsc;
+      }
+
+      oscillator.type = d;
     });
+  }
 
-  volumeSliderOsc.on("input", function() {
-    console.log(volumeSliderOsc.property("value"))
-    gainNodeOsc.gain.value = parseFloat(volumeSliderOsc.property("value"));
-  })
+  function setupOscillatorSliders() {
+    //options for controlling the oscillator
+    var oscOptions = {
+      "volume": {
+        "class": "osc-volume-slider",
+        "min": 0,
+        "max": 1,
+        "step": 0.05,
+        "value": volumeOsc
+      },
+      "frequency": {
+        "class": "osc-freq-slider",
+        "min": 20,
+        "max": 20000,
+        "step": 10,
+        "value": freqOsc
+      }
+    };
+
+    //add sliders to window with control options
+    var oscSliders = d3.selectAll(".osc-slider").append("input")
+      .data(d3.values(oscOptions))
+      .attr({
+        "class": function(d) { return d.class; },
+        "type": "range",
+        "min": function(d) { return d.min; },
+        "max": function(d) { return d.max; },
+        "step": function(d) { return d.step; },
+        "value": function(d) { return d.value; }
+      });
+
+    //change frequency or volume on slider value change
+    oscSliders.on("input", function(d) {
+      if (d.class == "osc-volume-slider") {
+        volumeOsc = parseFloat(this.value);
+
+        if (!mutedOsc) {
+          gainNodeOsc.gain.value = volumeOsc;
+        }
+
+      } else if (d.class == "osc-freq-slider") {
+        freqOsc = parseFloat(this.value);
+        oscillator.frequency.value = freqOsc;
+      }
+
+    });
+  }
+}
+
+//MICROPHONE CONTROL SETUP
+function setupMicControls() {
+    //unhide microhpone controls
+    d3.select(".mic-controls").style("visibility", "visible");
+
+  //initial microphone parameters
+  var volumeMic = 0.1,
+    mutedMic = false;
+
+  gainNodeOsc.gain.value = volumeMic;
+  setupMicButtons();
+  setupMicSlider();
+
+  function setupMicButtons() {
+    //create on/off buttons for microphone
+    var micOnOff = d3.select(".mic-btns").selectAll("button")
+      .data(["on", "off"])
+      .enter()
+      .append("button")
+      .attr({
+        "class": "col-xs-3 col-md-6 btn btn-default",
+        "type": "button"
+      })
+      .text(function(d) { return d; });
+
+    //toggle microphone on and off when clicked
+    micOnOff.on("click", function(d) {
+      var pressedBtn = d3.select(this);
+
+      if (pressedBtn.classed("active")) {
+        return;
+      } else if (!pressedBtn.classed("active")) {
+        micOnOff.classed("active", false);
+        pressedBtn.classed("active", true);
+
+        if (d === "on") {
+          mutedMic = false;
+          gainNodeMic.gain.value = volumeMic;
+        } else if (d == "off") {
+          mutedMic = true;
+          gainNodeMic.gain.value = 0;
+        }
+      }
+    });
+  }
+
+  function setupMicSlider() {
+
+    var micVolSlider = d3.select(".mic-slider").append("input")
+      .attr({
+        "class": "osc-freq-slider",
+        "type": "range",
+        "min": 0,
+        "max": 1,
+        "step": 0.05,
+        "value": volumeMic
+      });
+
+    micVolSlider.on("input", function() {
+      volumeMic = this.value;
+
+      if (!mutedMic) {
+        gainNodeMic.gain.value = volumeMic;
+      }
+    });
+  }
 }
 
 //VISUALIZATIONS
@@ -163,7 +282,7 @@ function setupVis() {
   //d3 function for parsing line data for frequenct spectrum
   var drawFreqSpectrum = d3.svg.line()
     .x(function(d,i) { return i * visWidth / bufferLength; })
-    .y(function(d) { return visHeight - d; })
+    .y(function(d) { return visHeight - 15 - d; })
     .interpolate("step");
 
   //create frequency paths for audio sources
